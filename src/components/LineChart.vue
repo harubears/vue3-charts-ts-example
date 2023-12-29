a11d516e9bbc42f6ed4fe46a14d4be2e36d6015d
harubears <haruki@harubears.com>
2023-12-29 15:44:14 +0900

    created LineChart

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,5 +1,66 @@
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue'
+import { Chart, Grid, Line as LineChartComponent  } from 'vue3-charts'
+import { Direction, ChartAxis } from 'vue3-charts/src/types'
+
+import { plByMonth } from '@/data'
+
+export default defineComponent({
+    name: 'LineChart',
+    components: { Chart, Grid, LineChartComponent },
+    setup() {
+        const data = ref(plByMonth)
+        const direction: Ref<Direction> = ref('horizontal')
+        const margin = ref({
+            left: 0,
+            top: 20,
+            right: 20,
+            bottom: 0
+        })
+
+        const axis: Ref<ChartAxis> = ref({
+            primary: {
+                domain: ['dataMin', 'dataMax'],
+                type: 'band',
+                format: (val: string) => {
+                    if (val === 'Feb') {
+                        return '😜'
+                    }
+                    return val
+                }
+            },
+            secondary: {
+                domain: ['dataMin', 'dataMax + 100'],
+                type: 'linear',
+                ticks: 8
+            }
+        })
+
+        return { data, direction, margin, axis }
+    }
+})
+</script>
+
 <template>
-    <div>
-        Line Chart
-    </div>
+    <Chart :size="{ width: 500, height: 420 }" :data="data" :margin="margin" :direction="direction" :axis="axis">
+
+        <template #layers>
+            <!-- Generates a dashed line with 2-pixel dashes and 2-pixel gaps as a Grid -->
+            <Grid strokeDasharray="2,2" />
+            <!-- Primary Line -->
+            <LineChartComponent :dataKeys="['name', 'pl']" />
+            <!-- Secondary Line -->
+            <LineChartComponent :dataKeys="['name', 'avg']" :lineStyle="{ stroke: 'red' }" type="step" />
+        </template>
+
+        <template #widgets>
+            <Tooltip borderColor="#48CAE4" :config="{
+                name: { hide: true },
+                pl: { color: '#0077b6' },
+                avg: { label: 'averange', color: 'red' },
+                inc: { hide: true }
+            }" />
+        </template>
+
+    </Chart>
 </template>
