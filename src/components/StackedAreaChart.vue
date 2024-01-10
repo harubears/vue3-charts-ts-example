1a498b6a5ffe53042b7e9e6ffb27df84775d03e0
harubears <haruki@harubears.com>
2024-01-10 10:55:06 +0900

    created Stacked Area Chart. But need to investigate the axis issue.

--- src/components/StackedAreaChart.vue
+++ src/components/StackedAreaChart.vue
@@ -1,5 +1,95 @@
+<script lang="ts">
+import { defineComponent, ref, Ref, provide } from 'vue'
+
+// Using aliases to avoid ESLint error:
+// 'no-reserved-component-names'
+import {
+    Chart,
+    Grid,
+    Tooltip,
+    Marker as ChartMarker,
+    Line as LineChartComponent,
+    Area as AreaChartComponent
+} from 'vue3-charts'
+import { Direction, ChartAxis } from 'vue3-charts/src/types'
+
+import { plByMonth } from '@/data'
+
+export default defineComponent({
+    name: 'LineChart',
+    components: { Chart, Grid, Tooltip, ChartMarker, LineChartComponent, AreaChartComponent },
+    setup() {
+        const data = ref(plByMonth)
+        const direction: Ref<Direction> = ref('horizontal')
+        const margin = ref({
+            left: 0,
+            top: 20,
+            right: 20,
+            bottom: 0
+        })
+
+        const axis: Ref<ChartAxis> = ref({
+            primary: {
+                domain: ['dataMin', 'dataMax + 100'],
+                type: 'band'    // show the every value of the data
+            },
+            secondary: {
+                domain: ['dataMin', 'dataMax + 100'],
+                type: 'linear',
+                ticks: 8
+            }
+        })
+
+        provide('layerProps', {
+            stacked: true, // enable stacking
+        });
+
+        return { data, direction, margin, axis }
+    }
+})
+</script>
+
 <template>
-    <div>
-        Stacked Area Chart    
-    </div>
+    <Chart :size="{ width: 500, height: 420 }" :data="data" :margin="margin" :direction="direction" :axis="axis">
+
+        <template #layers>
+            <Grid strokeDasharray="2,2" />
+
+            <AreaChartComponent :dataKeys="['name', 'pl']" type="monotone" :areaStyle="{ fill: 'url(#grad-pl)' }" />
+            <LineChartComponent :dataKeys="['name', 'pl']" type="monotone" :lineStyle="{ stroke: '#FF6347' }" />
+
+            <AreaChartComponent :dataKeys="['name', 'avg']" type="monotone" :areaStyle="{ fill: 'url(#grad-avg)' }" />
+            <LineChartComponent :dataKeys="['name', 'avg']" type="monotone" :lineStyle="{ stroke: '#3CB371' }" />
+
+            <AreaChartComponent :dataKeys="['name', 'inc']" type="monotone" :areaStyle="{ fill: 'url(#grad-inc)' }" />
+            <LineChartComponent :dataKeys="['name', 'inc']" type="monotone" :lineStyle="{ stroke: '#1E90FF' }" />
+
+            <ChartMarker :value="1000" label="Mean." color="green" :strokeWidth="2" strokeDasharray="6 6" />
+            <defs>
+                <linearGradient id="grad-pl" gradientTransform="rotate(90)">
+                    <stop offset="0%" stop-color="#FF6347" stop-opacity="1" />
+                    <stop offset="100%" stop-color="white" stop-opacity="0.8" />
+                </linearGradient>
+                <linearGradient id="grad-avg" gradientTransform="rotate(90)">
+                    <stop offset="0%" stop-color="#3CB371" stop-opacity="1" />
+                    <stop offset="100%" stop-color="white" stop-opacity="0.8" />
+                </linearGradient>
+                <linearGradient id="grad-inc" gradientTransform="rotate(90)">
+                    <stop offset="0%" stop-color="#1E90FF" stop-opacity="1" />
+                    <stop offset="100%" stop-color="white" stop-opacity="0.8" />
+                </linearGradient>
+
+
+            </defs>
+        </template>
+
+        <template #widgets>
+            <Tooltip borderColor="#48CAE4" :config="{
+                pl: { color: '#FF6347' },
+                avg: { label: 'averange', color: '#3CB371' },
+                inc: { color: '#1E90FF' }
+            }" />
+        </template>
+
+    </Chart>
 </template>
